30587823686d9afe82b8cbe0010d0996d1e396f8
liqiang28 <liqiang28@meituan.com>
2018-01-29 19:43:05 +0800

    test commit

--- src/main/java/firstServlet.java
+++ src/main/java/firstServlet.java
@@ -1,5 +1,18 @@
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * Created by liqiang28 on 2018/1/29.
  */
-public class firstServlet {
+public class firstServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        System.out.println("test Here");
+        resp.getOutputStream().println("adf");
+    }
+
 }
